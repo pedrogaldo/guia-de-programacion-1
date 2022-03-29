4d849a7bfbc89957e9556940245603f6497f6b93
pedrogaldo <galdo.pedro13@gmail.com>
2022-03-29 13:58:27 -0300

    ejercicio 2 y 3
    terminado ejercicio 2 y 3

---
+++ Assets/Scenes/ejercicio2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+
+        int suma = num1 + num2;
+        int producto = num1 * num2;
+
+        Debug.Log(suma);
+
+        Debug.Log(producto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scenes/ejercicio3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio3 : MonoBehaviour
+{
+    public string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola " + nombre);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
