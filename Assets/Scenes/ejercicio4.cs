417b8f376b0c933a91ecafc8342b82814cc8f6cc
pedrogaldo <galdo.pedro13@gmail.com>
2022-03-29 14:28:24 -0300

    ejercicios 4 y 5
    ejercicios 4 y 5 terminados

---
+++ Assets/Scenes/ejercicio4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio4 : MonoBehaviour
+{
+    public int numero1;
+    public int numero2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("La suma entre " + (numero1) + " y " + (numero2) + " da como resultado " + (numero1 + numero2));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/ejercicio5.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio5 : MonoBehaviour
+{
+    public float num1;
+    public float num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("el resultado de: " + (num1) + " / " + (num2) + " = " + (num1 / num2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
